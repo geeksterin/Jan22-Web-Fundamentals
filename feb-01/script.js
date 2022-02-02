b84aabab09228b3059c5027535c0fd0b09a3bc00
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-02-02 11:09:40 +0530

    feb-01

---
+++ feb-01/script.js
@@ -0,0 +1,35 @@
+
+var obj = {
+    "first_name": "Aayush",
+    "last_name": "Sinha",
+    "email": "aayush.aryan@me.com",
+    "website": "sdfhsdhfjds",
+    "k": "This is Key K's value",
+    "students": [
+        {
+            "name": "Name 1",
+            "marks": 15
+        },
+        {
+            "name": "Name 2",
+            "marks": 17
+        },
+        {
+            "name": "Name 3",
+            "marks": 19
+        },
+        {
+            "name": "Name 4",
+            "marks": 21
+        },
+        {
+            "name": "Name 5",
+            "marks": 0
+        },
+        {
+            "name": "Name 6",
+            "marks": 27
+        },
+    ],
+};
+
